193aa3048ad28d6dd76ac8cd18655d7d0d8d9c96
Karolina Ziejlo <karolinaziejlo@wp.pl>
2022-01-02 16:46:23 +0100

    Sum all products

--- Pierwsze_commity_Shopping.py
+++ Pierwsze_commity_Shopping.py
@@ -2,6 +2,7 @@ shopping = {
 "piekarnia": ["chleb","bułki","pączek"],
 "warzywniak": ["marchew","seler","rukola"]
 }
+Ilosc = 0
 
 print("Lista zakupów")
 
@@ -9,3 +10,6 @@ for shop in shopping:
     shopping[shop] = [product.capitalize() for product in shopping[shop]]   
     
     print(f"Idę do {shop.capitalize()} i kupuję tam {shopping[shop]}")
+    Ilosc += (len(shopping[shop]))
+
+print(f"W sumie kupuję {Ilosc} produktów")
